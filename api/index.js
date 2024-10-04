f39ab611b58583300fa3e21dc770ad5a0290009e
RaghavendraKushtagi <2022cs_raghavendrakushtagi_c@nie.ac.in>
2024-10-04 21:36:08 +0530

    Connection with database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connection with MongoDB Successful');
+}).catch((err) =>{
+    console.log("!!!Failed to connect with MongoDB!!!-->" + err);
+})
 
 const app = express();
 
