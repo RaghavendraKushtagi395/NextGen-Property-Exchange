6d08410a5fe91f6bd2f5f59e23f56fee8d9ac5b5
RaghavendraKushtagi <2022cs_raghavendrakushtagi_c@nie.ac.in>
2024-10-06 19:28:59 +0530

    Complete sign up page UI

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ export default function Header() {
                     <Link to='/'><li className='hidden sm:inline text-slate-700 hover:underline'> Home</li></Link>
                     <Link to='/about'><li className='hidden sm:inline text-slate-700 hover:underline'> About </li> </Link>
                     <Link to='/chat'><li className='hidden sm:inline text-slate-700 hover:underline'> Bob AI </li> </Link>
-                    <Link to='/sign-in'><li className=' text-slate-700 hover:underline'> Sign in</li></Link>
+                    <Link to='/signin'><li className=' text-slate-700 hover:underline'> Sign in</li></Link>
                     
                 </ul>
             </div>
