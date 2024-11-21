98667b03863099148bfa107e8d119e7d66f269f2
RaghavendraKushtagi <2022cs_raghavendrakushtagi_c@nie.ac.in>
2024-11-21 19:44:50 +0530

    Complete delete listing functionality

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import expess from 'express';
-import { createListing } from '../controllerLogic/listing.controller.js';
+import { createListing, deleteListing } from '../controllerLogic/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = expess.Router();
 
 
-router.post('/create', verifyToken ,createListing)
+router.post('/create', verifyToken ,createListing);
+router.delete('/delete/:id', verifyToken, deleteListing);
 
 export default router;
