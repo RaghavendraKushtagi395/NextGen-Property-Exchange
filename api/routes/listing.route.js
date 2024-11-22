275552cae02ca79b4b2b76e8b1047be5470daaff
RaghavendraKushtagi <2022cs_raghavendrakushtagi_c@nie.ac.in>
2024-11-22 16:35:24 +0530

    Create api route for update listings

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import expess from 'express';
-import { createListing, deleteListing } from '../controllerLogic/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllerLogic/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = expess.Router();
@@ -7,5 +7,6 @@ const router = expess.Router();
 
 router.post('/create', verifyToken ,createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
